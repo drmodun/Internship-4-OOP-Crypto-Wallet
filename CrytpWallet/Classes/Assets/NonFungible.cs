e2d6149ffb1f77bb8f3778785e796b555b5ee438
drmodun <jan.modun.st@gmail.com>
2022-12-05 03:10:39 +0100

    Added Update TO nft value, also almost messed up, will explain in pull request

--- CrytpWallet/Classes/Assets/NonFungible.cs
+++ CrytpWallet/Classes/Assets/NonFungible.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CrytpWallet.Classes.Global;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
@@ -22,14 +23,16 @@ namespace CrytpWallet.Assets
         {
             
             OldValueInDollar = ValueInDollar;
+            ValueInDollar = GlobalWallets.AllFungibleAssets.Find(x => x.Adress == ItsFungible).ValueInDollar * ValueInFungible;
+            
 
             //Mijenjanje vrijenosti radim izvan objekta pošto nemam access cijeloj listi pa ne mogu preko adrese dobiti želejeni objekt
         }
         public override void PrintAsset()
         {
+            var token = GlobalWallets.AllFungibleAssets.Find(x => x.Adress == ItsFungible);
             base.PrintAsset();
-            Console.WriteLine($"Vrijednost u fungible assetu: {ItsFungible}\n" +
-                $"\nKoličina Fungible asseta {ValueInFungible}" +
+            Console.WriteLine($"Vrijednost u fungible assetu: {ValueInFungible} {token.Label}\n" +
                 $"Mijenjanje vrijednosti: {(ValueInDollar - OldValueInDollar) / 100}%");
         }
     }
